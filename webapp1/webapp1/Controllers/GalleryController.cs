4cefd12eef107055ce3ab38ef6184336abd80f68
Liz Kolk <liz.kolk@zirmed.com>
2019-06-21 12:26:10 -0400

    created mvc core app

---
+++ webapp1/webapp1/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapp1.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public ActionResult Index()
+        {
+            string path = ("~/content/glitter.png");
+            byte[] imageByteData = System.IO.File.ReadAllBytes(path);
+            string imageBase64Data = Convert.ToBase64String(imageByteData);
+            string imageDataURL = string.Format("data:image/png;base64,{0}", imageBase64Data);
+            ViewBag.ImageData = imageDataURL;
+
+            return View();
+        }
+    }
+}
