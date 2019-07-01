c325284f9ebfb420bf902f55bc2322371179061f
Liz Kolk <liz.kolk@zirmed.com>
2019-07-01 13:13:23 -0400

    making my app listen to port 8080

--- webapp1/webapp1/Program.cs
+++ webapp1/webapp1/Program.cs
@@ -4,8 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Logging;
 
 namespace webapp1
@@ -14,7 +16,15 @@ namespace webapp1
     {
         public static void Main(string[] args)
         {
+            var config = new
+ConfigurationBuilder().AddEnvironmentVariables("").Build();
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+
             BuildWebHost(args).Run();
+            var host = new WebHostBuilder()
+                .UseUrls(url)
+                .Build();
+            host.Run(); 
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
